a364c2788733809cd721aff39ae2d717b3181c64
zachaller <zachaller@users.noreply.github.com>
2023-01-16 13:52:06 -0600

    feat: Implement loki argo-rollout plugin

--- README.md
+++ README.md
@@ -1,2 +1,102 @@
-# sample-rollouts-metric-plugin
-This contains an example plugin for use with Argo Rollouts plugin system
+# rollouts-plugin-loki
+This contains an example loki plugin for use with Argo Rollouts plugin system. This plugin allows you to use a loki query to check the new version app during the rollout.
+
+### Build
+
+To build a release build run the command below:
+```bash
+make build-loki-plugin
+```
+
+To build a debug build run the command below:
+```bash
+make build-loki-plugin-debug
+```
+
+### Attaching a debugger to debug build
+If using goland you can attach a debugger to the debug build by following the directions https://www.jetbrains.com/help/go/attach-to-running-go-processes-with-debugger.html
+
+You can also do this with many other debuggers as well. Including cli debuggers like delve.
+## Using a Metric Plugin
+
+There are two methods of installing and using an argo rollouts plugin. The first method is to mount up the plugin executable
+into the rollouts controller container. The second method is to use a HTTP(S) server to host the plugin executable.
+
+### Mounting the plugin executable into the rollouts controller container
+
+There are a few ways to mount the plugin executable into the rollouts controller container. Some of these will depend on your
+particular infrastructure. Here are a few methods:
+
+* Using an init container to download the plugin executable
+* Using a Kubernetes volume mount with a shared volume such as NFS, EBS, etc.
+* Building the plugin into the rollouts controller container
+
+Then you can use setup the configmap to point to the plugin executable. Example:
+
+```yaml
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: argo-rollouts-config
+data:
+  plugins: |-
+    metricProviderPlugins: >-
+    - name: "ManakinCubber/rollouts-plugin-loki"
+      location: "file://./my-custom-plugin"
+```
+
+### Using a HTTP(S) server to host the plugin executable
+
+Argo Rollouts supports downloading the plugin executable from a HTTP(S) server. To use this method, you will need to
+configure the controller via the `argo-rollouts-config` configmaps `pluginLocation` to an http(s) url. Example:
+
+```yaml
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: argo-rollouts-config
+data:
+  plugins: |-
+    metricProviderPlugins: >-
+    - name: "ManakinCubber/rollouts-plugin-loki"
+      location: "https://github.com/ManakinCubber/rollouts-plugin-loki/releases/download/v1.0.0/loki-plugin_1.0.0_linux_arm64"
+      sha256: "08f588b1c799a37bbe8d0fc74cc1b1492dd70b2c" #optional sha256 checksum of the plugin executable
+```
+
+### Sample Analysis Template
+
+An example for this sample plugin below :
+```
+apiVersion: argoproj.io/v1alpha1
+kind: AnalysisTemplate
+metadata:
+  name: example-analysis
+spec:
+  args:
+    - name: basicAuthUsername
+      valueFrom:
+        secretKeyRef:
+          key: username
+          name: secret-credential-loki
+    - name: basicAuthSecret
+      valueFrom:
+        secretKeyRef:
+          key: password
+          name: secret-credential-loki
+  metrics:
+    - count: 3
+      failureCondition: result[0] >= 0.10
+      failureLimit: 3
+      interval: 5m
+      name: example-analysis
+      provider:
+        plugin:
+          ManakinCubber/rollouts-plugin-loki:
+            address: https://logs.local
+            password: "{{ args.basicAuthSecret }}"
+            query: >
+              sum(rate({cluster="preprod", namespace="app-example"} |=
+              `ERROR` [5m]))
+            timeout: 40
+            username: "{{ args.basicAuthUsername }}"
+```
---
+++ internal/plugin/plugin.go
@@ -0,0 +1,202 @@
+package plugin
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/argoproj/argo-rollouts/utils/plugin/types"
+	"io"
+	"net/http"
+	"net/url"
+	"strconv"
+	"time"
+
+	"github.com/argoproj/argo-rollouts/metricproviders/plugin"
+	"github.com/argoproj/argo-rollouts/pkg/apis/rollouts/v1alpha1"
+	"github.com/argoproj/argo-rollouts/utils/evaluate"
+	metricutil "github.com/argoproj/argo-rollouts/utils/metric"
+	timeutil "github.com/argoproj/argo-rollouts/utils/time"
+	log "github.com/sirupsen/logrus"
+)
+
+const (
+	ApiQuery = "/loki/api/v1/query"
+)
+
+type RpcPlugin struct {
+	LogCtx log.Entry
+}
+
+type Config struct {
+	// Address is the HTTP address and port of the loki server
+	Address string `json:"address,omitempty" protobuf:"bytes,1,opt,name=address"`
+
+	// Username Loki instance username
+	Username string `json:"username,omitempty" protobuf:"bytes,2,opt,name=username"`
+
+	// Password Loki instance user password
+	Password string `json:"password,omitempty" protobuf:"bytes,3,opt,name=password"`
+
+	// Query is a raw loki query to perform
+	Query string `json:"query,omitempty" protobuf:"bytes,2,opt,name=query"`
+}
+
+// QueryResponse Loki response query
+type QueryResponse struct {
+	Status string `json:"status"`
+	Data   Data   `json:"data"`
+}
+
+// Data response detail
+type Data struct {
+	ResultType string       `json:"resultType"`
+	Result     []LokiResult `json:"result"`
+}
+
+// LokiResult result item
+type LokiResult struct {
+	Metric map[string]string `json:"metric,omitempty"`
+	Value  []json.RawMessage `json:"value,omitempty"`
+}
+
+func (g *RpcPlugin) InitPlugin() types.RpcError {
+	return types.RpcError{}
+}
+
+func (g *RpcPlugin) Run(analysisRun *v1alpha1.AnalysisRun, metric v1alpha1.Metric) v1alpha1.Measurement {
+	startTime := timeutil.MetaNow()
+	newMeasurement := v1alpha1.Measurement{
+		StartedAt: &startTime,
+	}
+
+	client := http.Client{Timeout: time.Duration(10) * time.Second}
+	config := Config{}
+	response := QueryResponse{}
+
+	err := json.Unmarshal(metric.Provider.Plugin["ManakinCubber/rollouts-plugin-loki"], &config)
+	if err != nil {
+		return metricutil.MarkMeasurementError(newMeasurement, err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+	defer cancel()
+
+	lokiUrl, err := url.Parse(config.Address + ApiQuery)
+	if err != nil {
+		return metricutil.MarkMeasurementError(newMeasurement, err)
+	}
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, lokiUrl.String(), nil)
+	if err != nil {
+		return metricutil.MarkMeasurementError(newMeasurement, err)
+	}
+
+	params := url.Values{}
+	params.Add("query", config.Query)
+	req.URL.RawQuery = params.Encode()
+
+	if config.Username != "" && config.Password != "" {
+		req.SetBasicAuth(config.Username, config.Password)
+	}
+
+	res, err := client.Do(req)
+	if err != nil {
+		return metricutil.MarkMeasurementError(newMeasurement, err)
+	}
+
+	body, err := io.ReadAll(res.Body)
+	defer res.Body.Close()
+	if res.StatusCode > 299 {
+		return metricutil.MarkMeasurementError(newMeasurement, fmt.Errorf("error fetching metrics: %s", res.Status))
+	}
+
+	if err := json.Unmarshal(body, &response); err != nil {
+		return metricutil.MarkMeasurementError(newMeasurement, err)
+	}
+
+	newValue, newStatus, err := g.processResponse(metric, response)
+	if err != nil {
+		return metricutil.MarkMeasurementError(newMeasurement, err)
+	}
+
+	newMeasurement.Value = newValue
+	newMeasurement.Phase = newStatus
+	finishedTime := timeutil.MetaNow()
+	newMeasurement.FinishedAt = &finishedTime
+	return newMeasurement
+}
+
+func (g *RpcPlugin) Resume(analysisRun *v1alpha1.AnalysisRun, metric v1alpha1.Metric, measurement v1alpha1.Measurement) v1alpha1.Measurement {
+	return measurement
+}
+
+func (g *RpcPlugin) Terminate(analysisRun *v1alpha1.AnalysisRun, metric v1alpha1.Metric, measurement v1alpha1.Measurement) v1alpha1.Measurement {
+	return measurement
+}
+
+func (g *RpcPlugin) GarbageCollect(*v1alpha1.AnalysisRun, v1alpha1.Metric, int) types.RpcError {
+	return types.RpcError{}
+}
+
+func (g *RpcPlugin) Type() string {
+	return plugin.ProviderType
+}
+
+func (g *RpcPlugin) GetMetadata(metric v1alpha1.Metric) map[string]string {
+	metricsMetadata := make(map[string]string)
+
+	config := Config{}
+	if err := json.Unmarshal(metric.Provider.Plugin["ManakinCubber/rollouts-plugin-loki"], &config); err != nil {
+		return nil
+	}
+	if config.Query != "" {
+		metricsMetadata["ResolvedLokiQuery"] = config.Query
+	}
+	return metricsMetadata
+}
+
+// Process the loki response query
+func (g *RpcPlugin) processResponse(metric v1alpha1.Metric, response QueryResponse) (string, v1alpha1.AnalysisPhase, error) {
+	switch response.Data.ResultType {
+	case "vector":
+		results := make([]float64, 0, len(response.Data.Result))
+		valueStr := "["
+		for _, s := range response.Data.Result {
+			if s.Value != nil {
+				for index, v := range s.Value {
+					if index > 0 {
+						itemValue := rawToString(v)
+						valueFloat, err := strconv.ParseFloat(itemValue, 64)
+						if err != nil {
+							return "", v1alpha1.AnalysisPhaseError, err
+						}
+						valueStr = valueStr + itemValue + ","
+						results = append(results, valueFloat)
+					}
+				}
+			}
+		}
+		// if we appended to the string, we should remove the last comma on the string
+		if len(valueStr) > 1 {
+			valueStr = valueStr[:len(valueStr)-1]
+		}
+		valueStr = valueStr + "]"
+		newStatus, err := evaluate.EvaluateResult(results, metric, g.LogCtx)
+		return valueStr, newStatus, err
+	default:
+		return "", v1alpha1.AnalysisPhaseError, fmt.Errorf("Loki log type not supported ")
+	}
+}
+
+// helper to decode RawMessage into string safely
+func rawToString(r json.RawMessage) string {
+	var s string
+	if err := json.Unmarshal(r, &s); err == nil {
+		return s
+	}
+	var n json.Number
+	if err := json.Unmarshal(r, &n); err == nil {
+		return n.String()
+	}
+	return string(r)
+}
